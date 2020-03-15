4ffd115ec85c5ba5d825524563bd30396779018f
Mehmet Tekman <mtekman89@gmail.com>
2020-03-16 00:16:30 +0100

    attempts to get it working with grep. Issues with reusing a buffer. Could be easier to do the parsing myself...

--- packagemap.el
+++ packagemap.el
@@ -4,17 +4,6 @@
 (defvar varprefix "(\\(def\\(var\\|un\\|subst\\)\\|setq\\) ")
 (defvar varfullix "\\(\(setq\|\(def\(un\|var\|subst\)\)\) \(-?\\w+\)+")
 
-(defmacro when-buffer-is-ready (buffname &rest rest)
-  "Checks if buffer BUFFNAME is fully populated, then performs REST on it."
-  `(add-hook
-    'ripgrep-search-finished-hook
-    '(lambda (x)
-       (with-current-buffer ,buffname
-         (sleep-for 1)
-         ,@rest))))
-       
-       
-
 (defmacro processline (nachcolon &rest rest)
   "Process a line, extract values between colons, and search for NACHCOLON
 immediately after second colon, then perform REST."
@@ -31,7 +20,7 @@ immediately after second colon, then perform REST."
                        (string= ,nachcolon nextchar))
              ,@rest))))))
 
-(defun getdefinition-from-line ()
+(defun process-this-line ()
   "Parse the current line in the ripgrep buffer to extract definitions.
 Specifically the filename, linenumber, and variable name."
   (save-excursion
@@ -50,7 +39,7 @@ Specifically the filename, linenumber, and variable name."
   "Get all references to toplevel definition VNAME."
   (let ((rmapp nil)
         (rbuff (projectile-ripgrep vname t)))
-    (when-buffer-is-ready rbuff
+    (with-current-buffer rbuff
       (goto-line 4)
       (while (search-forward "\n" nil t)
         (processline
@@ -63,16 +52,37 @@ Specifically the filename, linenumber, and variable name."
               (or '(getparentfunc filename linenumb) filename))))))))
 
 
+(setq topdefs nil)
+(setq grepbuffer nil)
+
+(defun killall-grep-buffers ()
+  (dolist
+      (buff (--filter
+             (string-prefix-p
+              "*grep"
+              (buffer-name it))
+             (buffer-list)))
+    (kill-buffer buff))
+  (kill-buffer "*Backtrace*"))
+
 (defun processdefinitions ()
   "Grab all definitions in a project, and process cross references."
-  (let ((rmapp nil)
-        (rbuff (projectile-ripgrep varfullix t)))
-    (when-buffer-is-ready rbuff
+  (add-hook 'projectile-grep-finished-hook 'processdefinitions-delegate)
+  (let ((nowbuff (current-buffer))
+        (pbuffer (projectile-grep
+                  "(\\(setq\\|\\(def\\(un\\|var\\|subst\\)\\)\\)\ " nil)))
+    (setq grepbuffer pbuffer)
+    (switch-to-buffer nowbuff)))
+
+(defun processdefinitions-delegate ()
+  (let ((bname grepbuffer))
+    (with-current-buffer bname
       (goto-line 4)
       (while (search-forward "\n" nil t)
-        (let* ((vdefs (getdefinition-from-line))
+        (let* ((vdefs (process-this-line))
                (vnams (car vdefs))
                ;;(xrefs (getcrossrefs-forvar vnams))
                )
-          (add-to-list 'rmapp `(,vdefs xrefs))))) ;;TODO: eval
-    rmapp))
+          (add-to-list 'topdefs `(,vdefs xrefs))))
+      (killall-grep-buffers)
+      (remove-hook 'ripgrep-search-finished-hook 'processdefinitions-delegate))))
