9657e4c148345bbfc8095148cb9186b2509fb073
Mehmet Tekman <mtekman89@gmail.com>
2020-03-20 02:58:18 +0100

    working on colors

--- package-map.el
+++ package-map.el
@@ -49,6 +49,62 @@
 
 
 
+(defun getcolors (hashtable accesskey1 &optional accesskey2)
+  "Get a list of colors."
+  (let ((uniqkeys nil)
+        (colors '("red" "blue" "green" "orange" "purple" "gray" "yellow" "pink")))
+    (maphash
+     (lambda (key annot)
+       (let ((val (plist-get annot accesskey1)))
+         (cl-pushnew val uniqkeys)))
+     hashtable)
+    (let ((numvals (length uniqkeys)))
+      (cl-subseq tmpcol 0 numvals))))
+
+
+(defun getshape (hashtable accesskey1 &optional accesskey2)
+  "Get a list of shapes."
+  (
+  
+  (let ((uniqkeys nil)
+        (colors '("red" "blue" "green" "orange" "purple" "gray" "yellow" "pink")))
+    (maphash
+     (lambda (key annot)
+       (let ((val (plist-get annot accesskey1)))
+         (cl-pushnew val uniqkeys)))
+     hashtable)
+    (let ((numvals (length uniqkeys)))
+      (cl-subseq tmpcol 0 numvals))))
+
+
+(defun makedotfile ()
+  (let ((hashtable (generatemap)))
+    ;; TODO: implement these
+    (let ((list-colors (getcolors hashtable :file))
+          (list-height (getheights hashtable :line-beg :line-end))
+          (list-shapes (getshapes hashtable :type)))
+      (with-current-buffer (find-file "graphviz2.dot")
+        (erase-buffer)
+        (insert "strict graph {\n")
+        (maphash
+         (lambda (funcname info)
+           (let ((vfile (plist-get info :file))
+                 (vbegs (plist-get info :line-beg))
+                 (vends (plist-get info :line-end))
+                 (vtype (plist-get info :type))
+                 (vment (plist-get info :mentions)))
+             (let ((numlines (if vends (- vends vbegs) 1)))
+               (insert (format "  \"%s\" [height=%f]\n"
+                               funcname (1+ (/ numlines 10)))))
+             (dolist (mento vment)
+               (unless (eq funcname mento)
+                 (insert (format "  \"%s\" -- \"%s\" [color=\"%s\"]\n"
+                                 funcname mento vfile))))))
+         hashtable)
+        (insert "}\n")))))
+
+
+
 ;; Logic:
 ;; -- if there is more than 1 file, then create several columns.
 ;; -- if only 1 file, more free for all approach.
